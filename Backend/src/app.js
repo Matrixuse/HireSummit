5177351f64c184a07af90ceb616fc4c55bd8adad
Developer <user@example.com>
2026-07-04 15:17:04 +0530

    Update backend app changes

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,7 +7,7 @@ app.use(cors({
     origin: [
         "http://localhost:5173",
         "http://localhost:5176",
-        "https://hiresummit-backend.onrender.com"
+        "https://hiresummit.onrender.com",
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
